49a35b95fd0e17745f4de0e28b27dce435835022
Jon Allgood <jon.allgood@gmail.com>
2017-01-25 11:19:24 -0800

    Lab 4-update

--- composite-dom.ts
+++ composite-dom.ts
@@ -8,7 +8,12 @@ class TextNode {
 }
 
 class DomElement {
+var currentNode,
+  ni = document.createNodeIterator(document.documentElement, NodeFilter.SHOW_ELEMENT);
 
-  let all = document.getElementsByTagName("*");
-
+while(currentNode=ni.nextNode()){
+  console.log(currentNode.nodeName);
+  }
 }
+
+//http://stackoverflow.com/questions/4256339/javascript-how-to-loop-through-all-dom-elements-on-a-page
